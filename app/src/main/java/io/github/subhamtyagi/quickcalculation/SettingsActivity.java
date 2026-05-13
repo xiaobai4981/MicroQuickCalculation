a609672f70f142fc90256e84473ba2401528f0fc
xiaobai <907242459@qq.com>
2026-05-13 15:41:26 +0800

    init

--- app/src/main/java/io/github/subhamtyagi/quickcalculation/SettingsActivity.java
+++ app/src/main/java/io/github/subhamtyagi/quickcalculation/SettingsActivity.java
@@ -28,10 +28,10 @@ public class SettingsActivity extends AppCompatActivity {
                     .replace(R.id.settings, new SettingsFragment())
                     .commit();
         }
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+//        ActionBar actionBar = getSupportActionBar();
+//        if (actionBar != null) {
+//            actionBar.setDisplayHomeAsUpEnabled(true);
+//        }
 
 
     }
